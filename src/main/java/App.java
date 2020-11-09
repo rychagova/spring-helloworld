197388b8692434ac5fe24dbc49da6636e08c9768
octob <rychagova.alexandra@gmail.com>
2020-11-09 22:15:19 +0300

    bean prototype

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -8,5 +8,16 @@ public class App {
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+        HelloWorld bean0 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(bean0.getMessage());
+
+        Cat bean1 = (Cat) applicationContext.getBean("cat");
+        System.out.println(bean1.getName());
+        Cat bean2 = (Cat) applicationContext.getBean("cat");
+        System.out.println(bean2.getName());
+
+        System.out.println(bean == bean0);
+        System.out.println(bean1 ==bean2);
     }
 }
